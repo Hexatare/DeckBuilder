47c5614f1362d21ad7e69388c90d664bdc9babd8
Hexatare <edu.nm@proton.me>
2023-09-18 15:32:06 +0200

    Added jd to send POST requests

--- deck_builder/__init__.py
+++ deck_builder/__init__.py
@@ -35,11 +35,13 @@ def init_app():
         # pylint: disable=import-outside-toplevel
         from .homepage import homepage_routes
         from .auth import auth_routes
+        from .dashboard import dashboard_routes
         # pylint: enable=import-outside-toplevel
 
         # Register blueprints
         app.register_blueprint(auth_routes.auth_bp)
         app.register_blueprint(homepage_routes.homepage_bp)
+        app.register_blueprint(dashboard_routes.dashboard_bp)
 
         # Create database models
         db.create_all()
