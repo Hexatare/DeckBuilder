9f4b92cde54576c0c76478e83cb5788e1ba5614d
qw3r-T <qqw33r.T@gmail.com>
2023-09-21 10:48:03 +0200

    add download button to editor

---
+++ deck_builder/editor/static/js/editor.js
@@ -0,0 +1,22 @@
+const downloadButton = document.getElementById('download-button');
+
+downloadButton.addEventListener('click', function() {
+  download_deck(1);
+});
+
+function download_deck(id) {
+  fetch("/editor/download/"+id)
+    .then(response => response.blob())
+    .then(blob => {
+      const blobURL = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = blobURL;
+      link.download = 'deck.txt';
+      link.style.display = 'none';
+      document.body.appendChild(link)
+      link.click();
+      window.URL.revokeObjectURL(blobURL);
+      document.body.removeChild(link);
+  });
+  console.log("function was executed")
+}
